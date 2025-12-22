97d3deddd7231e8139d044a86494260ef702d7e7
Matthias Gliwka <matthias@gliwka.eu>
2025-12-22 19:51:26 +0200

    feat: host website on bunny cdn

---
+++ website.tf
@@ -0,0 +1,52 @@
+locals {
+    website_domain = "gliwka.eu"
+}
+
+resource "bunnynet_storage_zone" "website_storage" {
+  name      = "gliwka-eu-storage"
+  region    = "DE"
+  zone_tier = "Edge"
+  replication_regions = ["NY", "LA", "SG", "SYD", "UK", "SE"]
+  custom_404_file_path = "/404.html"
+}
+
+resource "bunnynet_storage_zone" "website_log_storage" {
+  name      = "gliwka-eu-logs"
+  region    = "DE"
+  zone_tier = "Standard"
+  replication_regions = []
+}
+
+resource "bunnynet_pullzone" "website_cdn" {
+  name = "gliwka-eu-cdn"
+
+  log_enabled = true
+  log_storage_enabled = true
+  log_storage_zone = bunnynet_storage_zone.website_log_storage.id
+  log_anonymized = true
+  log_anonymized_style = "OneDigit" # Drop last octet
+
+  origin {
+    type = "StorageZone"
+    storagezone = bunnynet_storage_zone.website_storage.id
+  }
+
+  routing {
+    tier = "Standard"
+  }
+}
+
+resource "bunnynet_dns_record" "website_cdn_dns" {
+  zone = bunnynet_dns_zone.zone[local.website_domain].id
+  name  = ""
+  type  = "PullZone"
+  value = bunnynet_pullzone.website_cdn.name
+  pullzone_id = bunnynet_pullzone.website_cdn.id
+}
+
+resource "bunnynet_dns_record" "website_www_redirect" {
+  zone = bunnynet_dns_zone.zone[local.website_domain].id
+  name  = "www"
+  type  = "Redirect"
+  value = "https://${local.website_domain}"
+}
